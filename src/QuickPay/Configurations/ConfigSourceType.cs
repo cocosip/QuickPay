8b200846daa37017f3782660af2d7f4b552d896e
virtual <1185513330@qq.com>
2018-11-14 13:20:43 +0800

    去掉从数据库加载应用的配置,支付的时候,有哪些配置是固定的 Signed-off-by: virtual <1185513330@qq.com>

--- src/QuickPay/Configurations/ConfigSourceType.cs
+++ src/QuickPay/Configurations/ConfigSourceType.cs
@@ -6,10 +6,10 @@ namespace QuickPay.Configurations
     /// </summary>
     public enum ConfigSourceType
     {
-        /// <summary>从数据库获取配置
-        /// </summary>
-        [Description("从数据库获取配置")]
-        FromDb = 1,
+        ///// <summary>从数据库获取配置
+        ///// </summary>
+        //[Description("从数据库获取配置")]
+        //FromDb = 1,
 
         /// <summary>从配置文件中获取配置
         /// </summary>

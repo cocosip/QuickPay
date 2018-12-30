0e8028120b5faddd73aa3e5ff262fef63a33b072
virtual <1185513330@qq.com>
2018-12-30 09:27:40 +0800

    修改大小写名称 Signed-off-by: virtual <1185513330@qq.com>

--- core/src/QuickPay/WeChatPay/Responses/Common/DownloadBillResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace QuickPay.WeChatPay.Responses
-{
-    /// <summary>下载对账单
-    /// </summary>
-    public class DownloadBillResponse : WeChatPayCommonResponse
-    {
-
-    }
-}
--- core/src/QuickPay/WechatPay/Responses/Common/ReportResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace QuickPay.WeChatPay.Responses
-{
-    /// <summary>上报
-    /// </summary>
-    public class ReportResponse : WeChatPayCommonResponse
-    {
-    }
-}

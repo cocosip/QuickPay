62945ed8748a641da1a16622687a1b06c142cd48
virtual <1185513330@qq.com>
2020-01-06 10:42:57 +0800

    修复测试大小写问题 Signed-off-by: virtual <1185513330@qq.com>

---
+++ framework/test/QuickPay.Tests/WeChatPay/Util/WeChatPayUtilTest.cs
@@ -0,0 +1,23 @@
+﻿using QuickPay.WeChatPay.Utility;
+using Xunit;
+
+namespace QuickPay.Tests.WeChatPay.Util
+{
+    public class WeChatPayUtilTest
+    {
+        [Fact]
+        public void GenerateTimeStamp_Test()
+        {
+            var t = WeChatPayUtil.GenerateTimeStamp();
+            Assert.NotEmpty(t);
+        }
+
+        [Fact]
+        public void GenerateNonceStr_Test()
+        {
+            var nonceStr = WeChatPayUtil.GenerateNonceStr();
+            Assert.Equal(32, nonceStr.Length);
+        }
+
+    }
+}
